ea78bb3db5fa5594106f9674fe42d69c68f9a319
Иван <ivan@MacBook-Pro-de-Ivan.local>
2026-02-10 21:38:02 +0300

    зробив сторинку Категорії

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 // import "./App.css";
 import MainPage from "./pages/MainPage/MainPage";
-
+import CategoriePage from "./pages/СategoriesPage/CategoriesPage"
 function App() {
   return (
     <>
       <MainPage />
+      <CategoriePage/>
     </>
   );
 }
