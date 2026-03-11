606a78eede6e4a3ba9cffb721ae19b7ffeb48812
danilT9 <daniltrotsko9@gmail.com>
2026-03-11 22:13:21 +0300

    sign-menu

---
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,13 @@
+import SignHeader from "../../components/SignMenu/SignHeader";
+import RegisterMenu from "../../components/SignMenu/RegisterMenu/RegisterMenu";
+import SignFooter from "../../components/SignMenu/SignFooter";
+
+export default function RegisterPage() {
+  return (
+    <>
+      <SignHeader />
+      <RegisterMenu />
+      <SignFooter />
+    </>
+  );
+}
