98ace86f0e2e6452676727e13483765b62735a07
Mariya Dovgalyuk <162305105+MashaKarri@users.noreply.github.com>
2026-03-31 19:02:54 +0300

    reviews-adding

--- src/components/Reviews/modalRewiews.jsx
+++ src/components/Reviews/modalRewiews.jsx
@@ -2,17 +2,66 @@ import { Component } from "react";
 import styled from "./modalRewiews.module.css";
 
 export class RewiewsModal extends Component {
+  state = {
+    name: "",
+    text: "",
+  };
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!this.state.name || !this.state.text) return;
+
+    const newReview = {
+      name: this.state.name,
+      text: this.state.text,
+      product: "Це новий товар",
+    };
+
+    this.props.onAddReview(newReview);
+
+    this.setState({
+      name: "",
+      text: "",
+    });
+
+    this.props.onClose();
+  };
+
   render() {
     return (
       <div className={styled.backdrop} onClick={this.modModal}>
         <div className={styled.modal}>
           <button onClick={this.props.onClose}>X</button>
+
           <h1 className={styled.title}>Залишити відгук</h1>
-          <form className={styled.form}>
+
+          <form className={styled.form} onSubmit={this.handleSubmit}>
             <h3>Ваше ім'я</h3>
-            <input type="text" placeholder="Ваше ім'я" />
+            <input
+              type="text"
+              placeholder="Ваше ім'я"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+
             <h3>Ваш відгук</h3>
-            <textarea placeholder="Ваш відгук"></textarea>
+            <textarea
+              placeholder="Ваш відгук"
+              name="text"
+              value={this.state.text}
+              onChange={this.handleChange}
+            ></textarea>
+
             <button className={styled.button__submit}>Надіслати</button>
           </form>
         </div>
